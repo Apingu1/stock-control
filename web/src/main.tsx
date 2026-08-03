d8d97483b4c5e7b85e184817d03ba9fb8e981edf
Apingu1 <mumar1@live.co.uk>
2026-08-03 13:08:34 +0100

    feat: add installable stock control PWA

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./pwa.css";
+import App from "./App.tsx";
+import PwaUpdatePrompt from "./components/system/PwaUpdatePrompt";
+import { registerPwa } from "./pwa";
 
-createRoot(document.getElementById('root')!).render(
+registerPwa();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <PwaUpdatePrompt />
   </StrictMode>,
-)
+);
